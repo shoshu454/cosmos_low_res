d333b97ec4443173b5084b0ab69d1da1decd67d8
Joey Wang <joeyw3@vision-c22.cs.illinois.edu>
2026-04-15 16:33:36 -0500

    no bug right now

--- run_cos.sh
+++ run_cos.sh
@@ -29,15 +29,15 @@ PYTHONPATH=$(pwd) python scripts/dataproc_extract_frames_from_video.py \
     --input_folder "$TMP_VIDEO_IN" \
     --output_folder "$TMP_FRAMES_OUT" \
     --frame_rate 24 \
-    --resize 448x256 \
-    --max_frames 57
+    --resize 448x256
+    # --max_frames 57
 
 echo "2nd step"
 
 CUDA_HOME=$CONDA_PREFIX PYTHONPATH=$(pwd) python cosmos_predict1/diffusion/inference/inference_inverse_renderer.py \
     --checkpoint_dir checkpoints \
     --diffusion_transformer_dir Diffusion_Renderer_Inverse_Cosmos_7B \
-    --dataset_path "$EXTRACTED_PATH" \
+    --dataset_path "$TMP_FRAMES_OUT" \
     --video_save_folder "$TMP_INFERENCE_OUT" \
     --num_video_frames 57 \
     --group_mode folder \
@@ -49,4 +49,4 @@ echo ">>> Step 3: Analyzing..."
 find "$TMP_INFERENCE_OUT" -name "*.mp4" -exec cp {} "$FINAL_OUT_DIR/" \;
 
 rm -rf "$TMP_WORK_DIR"
-echo "Done, output dir: $FINAL_OUTPUT_DIR"
+echo "Done, output dir: $FINAL_OUT_DIR"
